bd4582970a1ea03b72b5fe637113d2e3ab1cf4d3
Saurabh0Gupta <saurabhgupta7814@gmail.com>
2024-03-20 18:55:10 +0530

    email regex

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ const authorSchema=mongoose.Schema({
     type:String,
     required:true,
     unique:true,
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please fill a valid email address']
   },
   password:{
     type:String,
